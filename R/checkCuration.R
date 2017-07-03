bb3d97bd870fec0ced183c524d6c0e4971e36881
Levi Waldron <lwaldron.research@gmail.com>
2017-07-03 14:55:44 -0400

    Now keeps track of columns correctly. Still needs to accommodate semicolon-delimited multiple entries.

--- R/checkCuration.R
+++ R/checkCuration.R
@@ -27,94 +27,49 @@
 #'
 checkCuration <- function(curated,
                           template = read.csv(system.file("extdata/template.csv",
-                              package = "curatedMetagenomicDataCuration"),
+                                                          package = "curatedMetagenomicDataCuration"),
                                               as.is = TRUE)) {
-    problems <- list(colnames = NULL, values = NULL)
-    if (is(curated, "ExpressionSet"))
-        curated <- pData(curated)
-    ##-------------------------------------------------
-    ##check that the column names match the template
-    ##-------------------------------------------------
-    if (identical(all.equal(colnames(curated), template$col.name), TRUE)) {
-        message("column names OK")
-    } else{
-        if (length(colnames(curated)) != template$col.name) {
-            warning(
-                paste(
-                    "There are",
-                    ncol(curated),
-                    "columns in curated, but",
-                    nrow(template),
-                    "columns defined by template."
-                )
-            )
-        } else{
-            warning(
-                "Type `View(problems$colnames)` to find mis-matched column names, assuming output of the function is named `problems`"
-            )
-            x <- colnames(curated)
-            x[!x == template$col.name] <-
-                paste0("!!!", x[!x == template$col.name], "!!!")
-            problems$colnames <-
-                cbind(x, template$col.name)
-            colnames(problems$colnames) <- c("curated", "template")
-        }
+  problems <- list(missingols = NULL, invalidcols = NULL, values = NULL)
+  ##-------------------------------------------------
+  ##check that the column names match the template
+  ##-------------------------------------------------
+  requiredcols <- template[template$requiredness=="required", "col.name"]
+  missingcols <- requiredcols[!requiredcols %in% colnames(curated)]
+  if (length(missingcols) > 0)
+    problems$missingols <- missingcols
+  invalidcols <- colnames(curated)[!colnames(curated) %in% template$col.name]
+  if (length(invalidcols) > 0)
+    problems$invalidcols <- invalidcols
+  ##-------------------------------------------------
+  ##construct the regexes from template$allowedvalues
+  ##-------------------------------------------------
+  template <- template[template$col.name %in% colnames(curated), , drop=FALSE]
+  curated <- curated[, match(template$col.name, colnames(curated)), drop=FALSE]
+  regexes <- template$allowedvalues
+  regexes <- paste("^", regexes, "$", sep = "")
+  regexes <- gsub("|", "$|^", regexes, fixed = TRUE)
+  regexes[grepl("^*$", regexes, fixed = TRUE)] <- "."
+  names(regexes) <- template$col.name
+  ##-------------------------------------------------
+  ##Check the data entries in each column for regex
+  ## matching, uniqueness, and missingness
+  ##-------------------------------------------------
+  all.ok <- TRUE
+  for (j in seq_along(colnames(curated))) {
+    doesmatch <- grepl(regexes[j], curated[, j])
+    if (template[j, "requiredness"] == "optional") {
+      doesmatch[is.na(curated[, j])] <- FALSE
     }
-    ##-------------------------------------------------
-    ##construct the regexes from template$allowedvalues
-    ##-------------------------------------------------
-    regexes <- template$allowedvalues
-    regexes <- paste("^", regexes, "$", sep = "")
-    regexes <- gsub("|", "$|^", regexes, fixed = TRUE)
-    ##regexes[template$requiredness=="optional"] <- paste(regexes[template$requiredness=="optional"],"|^NA$",sep="")
-    regexes[grepl("^*$", regexes, fixed = TRUE)] <- "."
-    names(regexes) <- template$col.name
-    if (any(grepl("decimal", regexes)))
-        regexes[grepl("decimal", regexes)] <- "^[0-9]+\\.?[0-9]*$"
-    if (any(grepl("integer", regexes)))
-        regexes[grepl("integer", regexes)] <- "^[0-9]+$"
-    if (any(grepl("decimalonly", regexes)))
-        regexes[grepl("decimalonly", regexes)] <-
-        "^[0-9]+\\.{1}[0-9]*$"
-    ##-------------------------------------------------
-    ##Check the data entries in each column for regex
-    ## matching, uniqueness, and missingness
-    ##-------------------------------------------------
-    ##check column names
-    column.OK <- rep(NA, ncol(curated))
-    names(column.OK) <- colnames(curated)
-    for (j in seq_along(1:ncol(curated))) {
-        doesmatch <- grep(regexes[j], curated[, j])
-        if (template[j, "requiredness"] == "optional") {
-            doesmatch <- c(doesmatch, which(is.na(curated[, j])))
-        }
-        doesnotmatch <- 1:nrow(curated)
-        doesnotmatch <- doesnotmatch[!doesnotmatch %in% doesmatch]
-        ## if field must be unique, add non-unique values to doesnotmatch
-        if (template[j, "uniqueness"] == "unique") {
-            counts.table <- table(curated[, j])
-            counts <-
-                counts.table[match(curated[, j], names(counts.table))]  #this counts the occurences
-            counts[is.na(counts)] <- 1  ##Consider NAs to be unique
-            nonunique <- which(counts > 1)
-            doesnotmatch <- c(doesnotmatch, nonunique)
-            doesnotmatch <-
-                unique(doesnotmatch)  #don't duplicate fields that fail both tests
-        }
-        if (length(doesnotmatch) == 0) {
-            column.OK[j] <- TRUE
-        } else{
-            warning(paste("Curation errors in column: ", colnames(curated)[j]))
-            column.OK[j] <- FALSE
-            curated[doesnotmatch, j] <-
-                paste("!!!", curated[doesnotmatch, j], "!!!", sep = "")
-        }
+    ## if field must be unique, add non-unique values to doesnotmatch
+    if (template[j, "uniqueness"] == "unique") {
+      doesmatch[!Biobase::isUnique( curated[, j] )] <- FALSE
     }
-    if (!all(column.OK)) {
-        warning(
-            "Type `View(problems$values)` and look for entries with !!!, assuming output of the function is named `problems`"
-        )
-        problems$values <- curated
-    }
-    return(problems)
+    curated[!doesmatch, j] <- paste("!!!", curated[!doesmatch, j], "!!!", sep = "")
+    if(!all(doesmatch)) all.ok <- FALSE
+  }
+  if(!all.ok)
+    problems$values <- curated
+  if(!identical(problems, list(missingols = NULL, invalidcols = NULL, values = NULL)))
+    warning("Curation problems found")
+  return(problems)
 }
