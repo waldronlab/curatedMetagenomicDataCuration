f7dffac9a8631bafd85c41f7eb0b852cb786c03c
Chloe Mirzayi <cmirzayi@gmail.com>
2020-05-08 08:58:34 -0600

    Adds get_metadata() function and associated documentation and dependencies

---
+++ R/get_metadata.R
@@ -0,0 +1,16 @@
+library(curl)
+library(jsonlite)
+
+#Get relevant metadata for each SRR using SRP
+get_metadata <- function(srp){
+  url_lookup <- paste('https://api.omicidx.cancerdatasci.org/sra/studies/', srp, '/runs?size=500', sep="")
+  srrs <- jsonlite::fromJSON(curl(url_lookup))$hits
+  df_srr <- data.frame(sampleID=srrs$sample_accession, SRR=srrs$accession,
+                       sequencing_platform=srrs$experiment$platform,
+                       number_reads =ifelse(srrs$experiment$library_layout=="PAIRED", as.numeric(srrs$total_spots)*2, srrs$total_spots),
+                       avg_read_length=srrs$avg_length)
+  return(df_srr)
+}
+
+
+
