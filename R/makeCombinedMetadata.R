751b0aaae0aec664e6529df8503ba0691b5d6092
cmirzayi <cmirzayi@gmail.com>
2021-03-12 10:53:06 -0700

    add MakeCombinedMetadata function

---
+++ R/makeCombinedMetadata.R
@@ -0,0 +1,22 @@
+#' Make a combined metadata table from individual TSVs
+#'
+#' @return A data.table containing metadata for all studies in inst/curated
+
+makeCombinedMetadata <- function(){
+  rbindlist(fetchTSVs(), fill=TRUE) %>% return()
+}
+
+.fetchTSVs <- function(){
+  studies <- system.file("curated", package = "curatedMetagenomicDataCuration") %>% 
+    list.files(recursive = TRUE, pattern="\\.tsv$", full.names = TRUE) %>% 
+    lapply(. %>% fetchStudy())
+  return(studies)
+}
+
+
+.fetchStudy <- function(filePath=""){
+  x <- read.table(filePath, header=TRUE, sep="\t")
+  studyName <- filePath %>% basename() %>% 
+    sub(x=., "_metadata.tsv", "", fixed=TRUE)
+  cbind(dataset_name=studyName, x) %>% return()
+}
