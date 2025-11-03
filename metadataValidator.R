6c56b60f669fe7bb4cf8d945bc7f869c44217230
krlong68 <krlong586@gmail.com>
2025-11-03 10:50:50 -0800

    Incorporate visualization into non-'validate' functions

--- metadataValidator.R
+++ metadataValidator.R
@@ -1,23 +1,24 @@
-library(ontologyIndex)
-library(xml2)
+#library(ontologyIndex)
+#library(xml2)
 library(validate)
 library(dplyr)
+library(tidyr)
+library(ggplot2)
 
-t <- get_ontology(file = "dictionaries/cmd_age_group.owl")
-t <- read_xml("dictionaries/cmd_age_group.owl")
+#t <- get_ontology(file = "dictionaries/cmd_age_group.owl")
+#t <- read_xml("dictionaries/cmd_age_group.owl")
 
 diet_dict <- read.csv("dictionaries/cmd_dietary_restriction.csv")
 color_dict <- c("red", "green", "blue", "yellow")
-validate_age <- function(x) {
-    if (abs(x)-round(x) != 0) {
-        return(FALSE)
-    }
-    if (x < 0 | x > 120) {
-       return(FALSE)
-    }
-
-    return(TRUE)
-}
+#validate_age <- function(x) {
+#    if (abs(x)-round(x) != 0) {
+#        return(FALSE)
+#    }
+#    if (x < 0 | x > 120) {
+#       return(FALSE)
+#    }
+#    return(TRUE)
+#}
 
 template <- list(id = function(x) !is.na(x),
                  dietary_restriction = diet_dict$label,
@@ -38,18 +39,17 @@ ddata <- data.frame(id = c(1, 2, NA, 4, 5),
 
 out <- confront(ddata, ttemplate)
 
-out2 <- validateMetadata(ddata, template)
-
 validate_column <- function(validation_type, allowed, data) {
     if (validation_type == "character") {
-        results <- unlist(lapply(data, function(x) x %in% allowed))
+        results <- unlist(lapply(data, function(x) if (is.na(x)) NA else x %in% allowed))
     } else if (validation_type == "function") {
-        results <- unlist(lapply(data, function(x) allowed(x)))
+        results <- unlist(lapply(data, function(x) if (is.na(x)) NA else allowed(x)))
     }
 
     rejected_ids <- which(!results)
     rejected <- data[rejected_ids]
-    report <- list(rejected_ids = rejected_ids,
+    report <- list(results = results,
+                   rejected_ids = rejected_ids,
                    rejected_values = rejected)
     return(report)
 }
@@ -73,13 +73,15 @@ validateMetadata <- function(data_table, template_list) {
     ## Validate columns
     # Non-matching columns
     nm_cols <- c(unvalidated_cols, missing_cols)
-    nm_results <- sapply(nm_cols, function(x) list(rejected_values = NA,
+    nm_results <- sapply(nm_cols, function(x) list(results = rep(NA, nrow(data_table)),
+                                                   rejected_values = NA,
                                                    rejected_ids = NA),
                          simplify = FALSE)
 
     # NA columns
     na_cols <- effective_template[names(which(is.na(effective_template)))]
-    na_results <- sapply(na_cols, function(x) list(rejected_values = as.character(),
+    na_results <- sapply(na_cols, function(x) list(results = rep(NA, nrow(data_table)),
+                                                   rejected_values = as.character(),
                                                    rejected_ids = as.integer()),
                          simplify = FALSE)
 
@@ -103,8 +105,34 @@ validateMetadata <- function(data_table, template_list) {
         rowwise() %>%
         dplyr::mutate(template = column %in% names(template_list),
                       data = column %in% colnames(data_table)) %>%
-        dplyr::mutate(rejected_values = list(all_results[[column]]$rejected_values),
+        dplyr::mutate(results = list(all_results[[column]]$results),
+                      rejected_values = list(all_results[[column]]$rejected_values),
                       rejected_ids = list(all_results[[column]]$rejected_ids))
 
     return(report)
 }
+
+plotValidation <- function(report) {
+    rdat <- as.data.frame(report$results)
+    names(rdat) <- report$column
+
+    pdat <- rdat %>%
+        pivot_longer(cols = everything(), names_to = "Variable", values_to = "Value") %>%
+        mutate(Value = factor(Value, levels = c(FALSE, TRUE, NA)))
+
+    ggplot(f, aes(x = Variable, fill = Value)) +
+        geom_bar(position = "fill") +
+        scale_y_continuous(labels = scales::percent) +
+        scale_fill_manual(values = c(palette.colors()[7], palette.colors()[4], palette.colors()[9])) +
+        labs(
+            title = "Validation Results",
+            x = "Column",
+            y = "Percent Validation Results",
+            fill = "Result"
+        ) +
+        coord_flip()
+}
+
+out2 <- validateMetadata(ddata, template)
+
+plotValidation(out2)
