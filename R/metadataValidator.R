f005d8f6872ce675b08259013c82b8219b6c988c
krlong68 <krlong586@gmail.com>
2025-12-18 16:22:59 -0800

    Remove old functions and clean up metadataValidator.R; Move implementation code to validate_metadata.qmd

--- R/metadataValidator.R
+++ R/metadataValidator.R
@@ -1,9 +1,4 @@
-library(validate)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(rdflib)
-library(stringr)
+# Function for validating curated metadata
 
 import_owl_id_label <- function(path) {
   ## Load info from file
@@ -29,141 +24,6 @@ import_owl_id_label <- function(path) {
   return(df)
 }
 
-validate_column <- function(validation_type, allowed, data) {
-  if (validation_type == "character") {
-    results <- unlist(lapply(data, function(x) if (is.na(x)) NA else x %in% allowed))
-  } else if (validation_type == "function") {
-    results <- unlist(lapply(data, function(x) if (is.na(x)) NA else allowed(x)))
-  }
-  
-  rejected_ids <- which(!results)
-  rejected <- data[rejected_ids]
-  report <- list(results = results,
-                 rejected_ids = rejected_ids,
-                 rejected_values = rejected)
-  return(report)
-}
-
-validateMetadata <- function(data_table, template_list) {
-  ## Validate input
-  # data_table
-  
-  # template_list
-  
-  ## Compare columns
-  unvalidated_cols <- setdiff(colnames(data_table), names(template_list))
-  missing_cols <- setdiff(names(template_list), colnames(data_table))
-  
-  effective_template <- template_list
-  effective_template[missing_cols] <- NULL
-  
-  ## Get validation classes
-  template_classes <- lapply(effective_template, class)
-  
-  ## Validate columns
-  # Non-matching columns
-  nm_cols <- c(unvalidated_cols, missing_cols)
-  nm_results <- sapply(nm_cols, function(x) list(results = rep(NA, nrow(data_table)),
-                                                 rejected_values = NA,
-                                                 rejected_ids = NA),
-                       simplify = FALSE)
-  
-  # NA columns
-  na_cols <- effective_template[names(which(is.na(effective_template)))]
-  na_results <- sapply(na_cols, function(x) list(results = rep(NA, nrow(data_table)),
-                                                 rejected_values = as.character(),
-                                                 rejected_ids = as.integer()),
-                       simplify = FALSE)
-  
-  # character columns
-  character_cols <- effective_template[names(which(template_classes == "character"))]
-  character_results <- sapply(names(character_cols), function(x) {
-    validate_column("character", character_cols[[x]], data_table[,x])
-  },
-  simplify = FALSE)
-  
-  # function columns
-  function_cols <- effective_template[names(which(template_classes == "function"))]
-  function_results <- sapply(names(function_cols), function(x) {
-    validate_column("function", function_cols[[x]], data_table[,x])
-  },
-  simplify = FALSE)
-  
-  ## Create report
-  all_results <- c(nm_results, na_results, character_results, function_results)
-  report <- data.frame(column = c(names(template_list), unvalidated_cols)) %>%
-    rowwise() %>%
-    dplyr::mutate(template = column %in% names(template_list),
-                  data = column %in% colnames(data_table)) %>%
-    dplyr::mutate(results = list(all_results[[column]]$results),
-                  rejected_values = list(all_results[[column]]$rejected_values),
-                  rejected_ids = list(all_results[[column]]$rejected_ids))
-  
-  return(report)
-}
-
-plotValidation <- function(report) {
-  rdat <- as.data.frame(report$results)
-  names(rdat) <- report$column
-  
-  pdat <- rdat %>%
-    pivot_longer(cols = everything(), names_to = "Variable", values_to = "Value") %>%
-    mutate(Value = factor(Value, levels = c(FALSE, TRUE, NA)))
-  
-  ggplot(pdat, aes(x = Variable, fill = Value)) +
-    geom_bar(position = "fill") +
-    scale_y_continuous(labels = scales::percent) +
-    scale_fill_manual(values = c(palette.colors()[7], palette.colors()[4], palette.colors()[9])) +
-    labs(
-      title = "Validation Results",
-      x = "Column",
-      y = "Percent Validation Results",
-      fill = "Result"
-    ) +
-    coord_flip()
-}
-
-#t <- import_owl(file = "inst/extdata/validation_dictionaries/cmd_age_group.owl")
-#t <- read_xml("dictionaries/cmd_age_group.owl")
-
-res <- import_owl_id_label("inst/extdata/validation_dictionaries/cmd_age_group.owl")
-
-diet_dict <- read.csv("inst/extdata/validation_dictionaries/cmd_dietary_restriction.csv")
-color_dict <- c("red", "green", "blue", "yellow")
-#validate_age <- function(x) {
-#    if (abs(x)-round(x) != 0) {
-#        return(FALSE)
-#    }
-#    if (x < 0 | x > 120) {
-#       return(FALSE)
-#    }
-#    return(TRUE)
-#}
-
-template <- list(id = function(x) !is.na(x),
-                 dietary_restriction = diet_dict$label,
-                 color = color_dict,
-                 age = function(x) {in_range(x, 0, 120) & abs(x)-round(x) == 0},
-                 species = is.character)
-
-#ttemplate <- validator(!is.na(id),
-#                       color %in% color_dict,
-#                       dietary_restriction %in% diet_dict$label,
-#                       in_range(age, 0, 120) & abs(age)-round(age) == 0)
-#names(ttemplate) <- c("id_exists", "color_dictionary", "diet_dictionary", "age_integer_in_range")
-
-ddata <- data.frame(id = c(1, 2, NA, 4, 5),
-                    color = c("red", "green", "blue", "blue", "red"),
-                    dietary_restriction = c("vegan", NA, "gluten", 1, "gluten"),
-                    age = c(27, 48, 34, 23.5, 199))
-
-#out <- confront(ddata, ttemplate)
-
-out2 <- validateMetadata(ddata, template)
-
-plotValidation(out2)
-
-## Functions
 check_required <- function(dict, data, include_all = FALSE) {
   # Which columns are required
   r_colnames <- dict$ColName[dict$Required == "required"]
@@ -208,7 +68,7 @@ check_required <- function(dict, data, include_all = FALSE) {
 
 is_integer_str <- function(x) grepl("^-?[0-9]+$", x)
 is_number_str <- function(x) !is.na(suppressWarnings(as.numeric(x)))
-#is_bool_str <- function(x) tolower(x) %in% c("true", "false", "t", "f", "yes", "no", "1", "0")
+is_bool_str <- function(x) tolower(x) %in% c("true", "false", "t", "f", "yes", "no", "1", "0")
 
 allow_na <- function(fn) {
   function(x) {
@@ -327,17 +187,6 @@ check_unique <- function(dict, data, include_all = FALSE) {
   return(results)
 }
 
-# Do we need this function?
-# If multiple not allowed, we just check if the full string is an allowed value.
-# If multiple allowed, we just split on the delimiter.
-check_multiple <- function(dict, data, include_all = FALSE) {
-  # Which columns do not allow multiple values
-  svals <- dict$ColName[dict$MultipleValues == FALSE]
-  
-  # Find intersecting columns
-  cols_to_check <- intersect(avals, colnames(data))
-}
-
 check_allowed_values <- function(dict, data, include_all = FALSE) {
   # Get columns with valid allowed values
   avals <- dict$ColName[!is.na(dict$AllowedValues) &
@@ -417,10 +266,26 @@ check_dictionary_values <- function(file_dir, dict, data, include_all = FALSE) {
   cols_to_check <- intersect(unique(c(names(ofiles), names(cfiles))),
                              colnames(data)) 
   
+  # Load preferred file for each column
+  patt_list <- lapply(cols_to_check, function(col) {
+    ofile <- ofiles[[col]]
+    cfile <- cfiles[[col]]
+    
+    if (!is.null(ofile)) {
+      pattern <- paste(import_owl_id_label(paste0(file_dir, ofile))$label,
+                       collapse = "|")
+    } else if (!is.null(cfile)) {
+      pattern <- paste(read.csv(paste0(file_dir, cfile))$label,
+                       collapse = "|")
+    }
+    
+    return(pattern)
+  })
+  
   # Check values
   results <- do.call(rbind, lapply(cols_to_check, function(col) {
-    ofile <- ofiles[[col]]
-    pattern <- dict$AllowedValues[dict$ColName == col]
+
+    
     required <- dict$Required[dict$ColName == col]
     delim <- dict$Delimiter[dict$ColName == col]
     
@@ -431,7 +296,7 @@ check_dictionary_values <- function(file_dir, dict, data, include_all = FALSE) {
       if (is.na(val) && na_allowed) return(TRUE)
       if (pattern == "") return(TRUE)
       vals <- unlist(strsplit(val, delim))
-      all(grepl(paste0("^", gsub(";", "|", pattern), "$"), vals))
+      all(grepl(paste0("^", pattern, "$"), vals))
     })
     
     if (include_all) {
@@ -472,35 +337,3 @@ check_dictionary_values <- function(file_dir, dict, data, include_all = FALSE) {
   
   return(results)
 }
-
-## Run test validation
-# Import cMD data dictionary
-cdd <- read.csv("inst/extdata/cMD_data_dictionary.csv",
-                colClasses = "character")
-
-# Load in test studies
-ts <- read.delim("inst/curated/ArtachoA_2021/ArtachoA_2021_metadata.tsv",
-                 colClasses = "character")
-
-as_names <- list.files("inst/curated", pattern = "_metadata.tsv",
-                       recursive = TRUE, full.names = TRUE)
-as <- lapply(as_names, function(x) read.delim(x, colClasses = "character"))
-names(as) <- gsub("_metadata.tsv", "", basename(as_names))
-
-data <- as[[3]]
-
-report <- dplyr::bind_rows(
-  check_required(cdd, ts),
-  check_class(cdd, ts),
-  check_unique(cdd, ts),
-  check_allowed_values(cdd, ts)
-)
-
-full_report <- dplyr::bind_rows(
-  check_required(cdd, ts, include_all = TRUE),
-  check_class(cdd, ts, include_all = TRUE),
-  check_unique(cdd, ts, include_all = TRUE),
-  check_allowed_values(cdd, ts, include_all = TRUE)
-)
-
-
