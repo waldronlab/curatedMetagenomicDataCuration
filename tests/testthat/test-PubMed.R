77f5a24a708e4e2220d9b5ce79830166a9540706
Sehyun Oh <shbrief@gmail.com>
2026-02-09 10:46:41 -0500

    remove old tests

--- tests/testthat/test-PubMed.R
+++
@@ -1,4 +0,0 @@
-context("PubMed")
-
-test_that("PubMed returns PubMed class s4 object",
-          expect_s4_class(PubMed("29088129"), "PubMed"))
--- tests/testthat/test-get_metadata.R
+++
@@ -1,4 +0,0 @@
-context("get_metadata")
-
-test_that("get_metadata returns metadata data.frame",
-          expect_s3_class(get_metadata("SRP082656"), "data.frame"))
