177006bd24a2e8762530e0170d1808ed0bb392bf
Sehyun Oh <shbrief@gmail.com>
2026-03-09 23:55:03 -0400

    update the function to select only passed curations

--- R/makeCombinedMetadata.R
+++ R/makeCombinedMetadata.R
@@ -6,6 +6,9 @@
 #' table, derived from the filename (e.g. \code{StudyA_2021_sample.tsv}
 #' becomes \code{StudyA_2021}).
 #'
+#' @param include_before_harmonized If it is set to `TRUE`, the returned 
+#' table will include not-harmonized (i.e., not-covered by the schema) 
+#' attributes as well. Default is `FALSE`.
 #' @return A \code{\link[data.table]{data.table}} containing sample-level
 #'   metadata for all studies, with columns filled with \code{NA} where a
 #'   study does not include a particular field.
@@ -18,16 +21,31 @@
 #'
 #' @importFrom data.table fread rbindlist :=
 #' @export
-makeCombinedMetadata <- function() {
+makeCombinedMetadata <- function(include_before_harmonized = FALSE) {
+  
   tsv_files <- list.files(
     system.file("curated", package = "curatedMetagenomicDataCuration"),
     recursive = TRUE,
     pattern = "_sample\\.tsv$",
     full.names = TRUE
   )
+  
   tables <- lapply(tsv_files, function(f) {
     x <- fread(f)
     x[, dataset_name := sub("_sample.tsv", "", basename(f), fixed = TRUE)]
   })
-  rbindlist(tables, fill = TRUE)
+  
+  all <- rbindlist(tables, fill = TRUE)
+  
+  if (isFALSE(include_before_harmonized)) {
+    extDir <- system.file("extdata", package = "curatedMetagenomicDataCuration")
+    harmonized_cols <- read.csv(file.path(extDir, "cMD_data_dictionary.csv")) %>% pull(col.name)
+    all <- all %>% dplyr::select(any_of(harmonized_cols))
+  } 
+  
+  key_id_cols <- c("study_name", "sample_id", "subject_id", "target_condition")
+  all_reordered <- all %>% 
+    select(any_of(key_id_cols), sort(setdiff(names(.), key_id_cols)))
+
+  return(all_reordered)
 }
